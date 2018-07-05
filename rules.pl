e14746b150a5ee1e3705c32a14f7a8b172752c0e
Vladimir Putin <der.edgar@gmx.de>
2018-07-05 10:18:47 +0200

    Possible Wall und Possible Trap Prädikate implementiert

--- rules.pl
+++ rules.pl
@@ -11,13 +11,21 @@ gold(X,Y) :- glitter(X, Y).
 wall(X,Y) :- false.
 
 % Wann ist ein Feld eine mögliche Wand
-possible_wall(X,Y) :- false.
-
+possible_wall(X,Y) :-
+	X1 is X-1, not(visited(X, Y)), bump(X1, Y);
+	X2 is X+1, not(visited(X, Y)), bump(X2, Y);
+	Y1 is Y-1, not(visited(X, Y)), bump(X, Y1); 
+	Y2 is Y+1, not(visited(X, Y)), bump(X, Y2).
+	
 % Wann ist ein Feld eine sichere Falle
 trap(X,Y) :- false.
 
 % Wann ist ein Feld eine mögliche Falle
-possible_trap(X,Y) :- false.
+possible_trap(X,Y) :- 
+	X1 is X-1, breeze(X1, Y);
+	X2 is X+1, breeze(X2, Y); 
+	Y1 is Y-1, breeze(X, Y1); 
+	Y2 is Y+1, breeze(X, Y2).
 
 % Wann ist ein Feld ein möglicher Wumpus
 wumpus(X,Y,ID) :- false.
